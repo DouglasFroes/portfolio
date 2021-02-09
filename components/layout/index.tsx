ed2c82363cea71a30b0cc16b51a2a284c09c1b34
Douglas Santos <douglassantos@MacBook-Pro-de-Douglas.local>
2021-02-09 13:48:54 -0300

    menu

---
+++ components/layout/index.tsx
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+import Head from "next/head";
+import styles from "../layout.module.css";
+import Link from "next/link";
+
+import * as styled from './styled'
+
+const name = "Douglas froes";
+export const siteTitle = "Portfolio Douglas S. Froes";
+
+export default function Layout({ children, home }) {
+  const [menu, setMenu] = useState(false)
+  const test = {
+    display: 'inline-block',
+    width: 320,
+    transform: 'translateX(0)'
+  }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"
+          rel="stylesheet"
+        />
+        <link href="/fontawesome/css/all.css" rel="stylesheet"></link>
+      </Head>
+      <body>
+        <styled.menuResponsive>
+          <i className="fa fa-bars" id="menu-open" onClick={() => setMenu(!menu)}></i>
+          <styled.menuDiv style={menu ? test : {}} >
+            <div id="menu-close" style={menu ? {} : { display: 'none' }}>
+              <i className="fa fa-times" id="menu-open2" onClick={() => setMenu(!menu)}></i>
+            </div>
+            <styled.conteiner>
+              <styled.imageDiv>
+                <Link href="/">
+                  <styled.imageRender src='https://avatars.githubusercontent.com/u/47641739?s=460&u=60d4e21d8f03d660978cef630b88284610261608&v=4' />
+                </Link>
+              </styled.imageDiv>
+              <styled.authDiv>
+                <styled.authH4>Douglas S. Froes</styled.authH4>
+                <styled.authSpan>Programador Full Stack</styled.authSpan>
+              </styled.authDiv>
+              <styled.menuNav role="navigation">
+                <ul>
+                  <li><Link href="/"><p>Sobre</p></Link></li>
+                  <li><Link href="/main"><p>Habilidades</p></Link></li>
+                  <li><Link href="/"><p>Meu trabalho</p></Link></li>
+                  <li><Link href="/"><p>Contate-me</p></Link></li>
+                </ul>
+              </styled.menuNav>
+              <styled.socialDiv>
+                <ul >
+                  <li>
+                    <a href="https://fb.com/templatemo">
+                      <i className="fab fa-facebook-square"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fab fa-whatsapp"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fa fa-rss"></i></a>
+                  </li>
+                </ul>
+              </styled.socialDiv>
+              <styled.bottomDiv>
+                <p>Copyright 2021 Douglas Froes</p>
+              </styled.bottomDiv>
+            </styled.conteiner>
+          </styled.menuDiv >
+        </styled.menuResponsive>
+
+        <main>{children}</main>
+        {
+          !home && (
+            <div className={styles.backToHome}>
+              <Link href="/">
+                <a>← Back to home</a>
+              </Link>
+            </div>
+          )
+        }
+      </body >
+    </div >
+  );
+}
---
+++ components/layout/styled.ts
@@ -0,0 +1,205 @@
+import styled from 'styled-components'
+
+export const menuResponsive = styled.div`
+ #menu-open{
+    display: none;
+    z-index: 999;
+    cursor: pointer;
+    position: fixed;
+    top: 0;
+    left: 0;
+    color: #a43f49;
+    background-color: #fff;
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+  }
+  #menu-open2{
+    z-index: 999;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    color: #a43f49;
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  #menu-close{
+    width:100%;
+    height:60px;
+    background-color:#fff;
+  }
+
+@media screen and (max-width: 845px)  {
+  #menu-open{
+    display: inline-block;
+  }
+}
+`
+
+export const menuDiv = styled.div`
+  left: 10%;
+   -webkit-transition: -webkit-transform 233ms cubic-bezier(0, 0, 0.21, 1);
+  -webkit-overflow-scrolling: touch;
+  background-color: #a43f49;
+  box-sizing: border-box;
+  height:100%;
+  max-height: 100%;
+  max-width: 80vw !important;
+  min-width: 45px !important;
+  outline: none;
+  overflow-x: hidden !important;
+  overflow-y: auto !important;
+  padding: 0;
+  position: fixed !important;
+  top: calc(0px);
+  width: 20%;
+  will-change: transform;
+  z-index: 9999 !important;
+  text-align: center;
+  overflow-y: scroll;
+
+  @media screen and (max-width: 1280px) and (min-width: 846px){
+    width: 25%;
+  }
+  @media screen and (max-width: 1280px){
+    left: 0;
+  }
+  ::-webkit-scrollbar {
+  display: none;
+  }
+  @media screen and (max-width: 845px){
+    width: 320px;
+    -webkit-transform: translateX(-100%);// !important;
+    transform: translateX(-100%);// !important;
+    transition: -webkit-transform 233ms cubic-bezier(0, 0, 0.21, 1);
+    transition: transform 233ms cubic-bezier(0, 0, 0.21, 1);
+    transition: transform 233ms cubic-bezier(0, 0, 0.21, 1), -webkit-transform 233ms cubic-bezier(0, 0, 0.21, 1);
+  }
+`;
+
+export const conteiner = styled.div`
+  padding-left: 0;
+  padding-right: 0;
+  margin: 0 auto;
+  display: block;
+`;
+
+export const imageDiv = styled.div`
+margin-top: 60px;
+display: flex;
+justify-content:center;
+`;
+
+export const imageRender = styled.img`
+    width: 140px;
+    height: 140px;
+    border-radius: 50%;
+`;
+
+export const authDiv = styled.div`
+  margin: 40px 4px 60px;
+`;
+
+export const authH4 = styled.h4`
+  font-size: 22px;
+    font-weight: 700;
+    color: #fff;
+    letter-spacing: 0.25px;
+    margin-bottom: 0;
+    margin-top: 0;
+`;
+
+export const authSpan = styled.span`
+font-size: 13px;
+    font-style: italic;
+    color: #fff;
+`
+
+export const menuNav = styled.nav`
+    text-align: center;
+    display: block;
+
+    ul{
+      padding: 0;
+    margin: 0;
+    list-style: none;
+    };
+    li{
+      display: block;
+    };
+    li p{
+    height: 70px;
+    line-height: 70px;
+    display: inline-block;
+    font-size: 16px;
+    font-weight: 700;
+    color: #fff;
+    width: 100%;
+    transition: all 0.5s;
+    border-top: 2px solid #fff;  
+    margin: 0;
+    };
+
+ li:last-child p {
+    border-bottom: 2px solid #fff;
+}
+`
+export const socialDiv = styled.div`
+    margin-top: 60px;
+    ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+ ul li {
+    display: inline-block;
+    margin:0;
+  }
+  
+  ul li a {
+    margin-right:3px;
+    width: 40px;
+    height: 40px;
+    display: inline-block;
+    text-align: center;
+    line-height: 40px;
+    background-color: rgba(0, 0, 0, 0.2);
+    color: #fff;
+    font-size: 14px;
+    transition: all 0.5s;
+  }
+
+  @media screen and (max-width: 1680px) and (min-width: 846px){
+    ul li a {
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+  }
+  ul li {
+    margin: 0;
+  }
+}
+`
+
+export const bottomDiv = styled.div`
+    position: relative;
+    margin-top: 60px;
+
+    p {
+    border-top: 1px solid rgba(250, 250, 250, 0.1);
+    display: inline-block;
+    width: 100%;
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-top: 30px;
+    font-size: 13px;
+    line-height: 24px;
+    color: #fff;
+    margin-top: 0;
+    margin-bottom: 1rem;
+}
+`;
