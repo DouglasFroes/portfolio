896c1f70a17bb2d87ba541b72f6b5740301e60ed
Douglas Froes <douglasfroes@Douglass-iMac-Pro.local>
2021-04-09 00:04:08 -0300

    meta

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -22,13 +22,11 @@ const Layout: React.FC<any> = ({ children, page }) => {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Portfolio de apresentação de trabalhos e principais habilidades"
         />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="https://avatars.githubusercontent.com/u/47641739?s=460&u=60d4e21d8f03d660978cef630b88284610261608&v=4"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
